1175803f5296dfd6d6f88b1efcf77adcbfb3a53e
Brian Su <briansuaccs@email.com>
2025-02-12 17:34:03 -0800

    removed double lrr-annotation and seperate from main repo

--- scripts/Run_lrr_annotation.py
+++ scripts/Run_lrr_annotation.py
@@ -14,33 +14,33 @@ def main():
 
     # Load or retrieve data
     if pdb_cached:
-        L.retrieve('../LRR_Annotation/cache')
+        L.retrieve('./LRR_Annotation/cache')
     else:
         # Changed input directory to use modeled structures
-        L.load_batch('../out_data/modeled_structures/pdb_for_lrr_annotator/')
+        L.load_batch('./out_data/modeled_structures/pdb_for_lrr_annotator/')
         
     if geometry_cached:
-        A.retrieve_geometry('../LRR_Annotation/cache')
+        A.retrieve_geometry('./LRR_Annotation/cache')
     else:
         A.load_structures(L.structures)
         A.compute_windings()
         
     if regressions_cached:
-        A.retrieve_regressions('../LRR_Annotation/cache')
+        A.retrieve_regressions('./LRR_Annotation/cache')
     else:
         A.compute_regressions()
 
     # Cache data if requested
     if make_cache:
-        L.cache('../LRR_Annotation/cache')
-        A.cache_geometry('../LRR-LRR_Annotation/cache')
-        A.cache_regressions('../LRR_Annotation-Annotation/cache')
+        L.cache('./LRR_Annotation/cache')
+        A.cache_geometry('./LRR_Annotation/cache')
+        A.cache_regressions('./LRR_Annotation/cache')
 
     # Load data into plotter
     P.load(A.windings, A.breakpoints, A.slopes)
 
     # Generate and save plots
-    P.plot_regressions(save=True, directory='../out_data/lrr_annotation_plots')
+    P.plot_regressions(save=True, directory='./out_data/lrr_annotation_plots')
 
 if __name__ == "__main__":
     main() 
