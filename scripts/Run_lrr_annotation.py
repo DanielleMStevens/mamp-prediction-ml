16ff5727a34961b98ac42673eba7ab8adf152aff
Danielle Stevens <dmstev@ucdavis.edu>
2025-02-12 17:25:25 -0800

    remove extra files in lrr-annotation

--- scripts/Run_lrr_annotation.py
+++ scripts/Run_lrr_annotation.py
@@ -14,27 +14,27 @@ def main():
 
     # Load or retrieve data
     if pdb_cached:
-        L.retrieve('../LRR-Annotation/cache')
+        L.retrieve('../LRR_Annotation/cache')
     else:
         # Changed input directory to use modeled structures
         L.load_batch('../out_data/modeled_structures/pdb_for_lrr_annotator/')
         
     if geometry_cached:
-        A.retrieve_geometry('../LRR-Annotation/cache')
+        A.retrieve_geometry('../LRR_Annotation/cache')
     else:
         A.load_structures(L.structures)
         A.compute_windings()
         
     if regressions_cached:
-        A.retrieve_regressions('../LRR-Annotation/cache')
+        A.retrieve_regressions('../LRR_Annotation/cache')
     else:
         A.compute_regressions()
 
     # Cache data if requested
     if make_cache:
-        L.cache('../LRR-Annotation/cache')
-        A.cache_geometry('../LRR-Annotation/cache')
-        A.cache_regressions('../LRR-Annotation/cache')
+        L.cache('../LRR_Annotation/cache')
+        A.cache_geometry('../LRR-LRR_Annotation/cache')
+        A.cache_regressions('../LRR_Annotation-Annotation/cache')
 
     # Load data into plotter
     P.load(A.windings, A.breakpoints, A.slopes)
