f1b6994aa61c8dc58c13419c689cfac2ef9a557c
Brian Su <briansuaccs@email.com>
2025-05-07 17:01:18 -0700

    update folder structure plus adding script for processing validation dataset

---
+++ 07_model_results/04_random_esm2_with_receptor_05_datasets/model_train_commands.sh
@@ -0,0 +1,3 @@
+python 06_scripts_ml/06_main_train.py --model esm2_with_receptor --data_dir 05_datasets --device cpu --epochs 20
+
+# datasets: *_data_with_all_test_random.csv
---
+++ 09_testing_and_validation/01_convert_sheet_to_fasta.R
@@ -0,0 +1,76 @@
+#-----------------------------------------------------------------------------------------------
+# Krasileva Lab - Plant & Microbial Biology Department UC Berkeley
+# Author: Danielle M. Stevens
+# Last Updated: 07/06/2020
+# Script Purpose: 
+# Inputs: 
+# Outputs: 
+#-----------------------------------------------------------------------------------------------
+
+library(readxl)
+library(tidyverse)
+
+######################################################################
+#  function to turn dataframe (where one column is the name and one column is the sequence)
+#   into a fasta file
+######################################################################
+
+writeFasta <- function(data, filename){
+  fastaLines = c()
+  for (rowNum in 1:nrow(data)){
+    fastaLines = c(fastaLines, data[rowNum,1])
+    fastaLines = c(fastaLines,data[rowNum,2])
+  }
+  fileConn<-file(filename)
+  writeLines(fastaLines, fileConn)
+  close(fileConn)
+}
+
+######################################################################
+# filter through blast results, filter by annotation, and put into distict fasta files
+######################################################################
+
+load_training_ML_data <- readxl::read_xlsx(path = "./02_in_data/All_LRR_PRR_ligand_data.xlsx", sheet = "Data_for_dropout_test")
+load_training_ML_data <- data.frame(load_training_ML_data)[1:12]
+
+receptor_full_length <- data.frame("Locus_Tag_Name" = character(0), "Sequence" = character(0))
+for (k in 1:nrow(load_training_ML_data)){
+  receptor_full_length <- rbind(receptor_full_length, data.frame(
+        "Locus_Tag_Name" = paste(paste(">",  load_training_ML_data$Plant.species[k], sep=""), 
+                                 load_training_ML_data$Locus.ID.Genbank[k], 
+                                 load_training_ML_data$Receptor[k], 
+                                 sep = "|"),
+        "Sequence" = load_training_ML_data$Receptor.Sequence[k])
+      )
+}
+  
+receptor_full_length <- receptor_full_length %>% distinct(Sequence, .keep_all = TRUE)
+writeFasta(receptor_full_length, "./09_testing_and_validation/dropout_case/receptor_full_length_dropout_test.fasta")
+
+
+######################################################################
+# filter through blast results, filter by annotation, and put into distict fasta files
+######################################################################
+
+load_training_ML_data <- readxl::read_xlsx(path = "./02_in_data/All_LRR_PRR_ligand_data.xlsx", sheet = "Model_Validation")
+load_training_ML_data <- data.frame(load_training_ML_data)[1:12]
+
+receptor_full_length <- data.frame("Locus_Tag_Name" = character(0), "Sequence" = character(0))
+for (k in 1:nrow(load_training_ML_data)){
+  receptor_full_length <- rbind(receptor_full_length, data.frame(
+        "Locus_Tag_Name" = paste(paste(">",  load_training_ML_data$Plant.species[k], sep=""), 
+                                 load_training_ML_data$Locus.ID.Genbank[k], 
+                                 load_training_ML_data$Receptor[k], 
+                                 sep = "|"),
+        "Sequence" = load_training_ML_data$Receptor.Sequence[k])
+      )
+}
+  
+receptor_full_length <- receptor_full_length %>% distinct(Sequence, .keep_all = TRUE)
+writeFasta(receptor_full_length, "./09_testing_and_validation/test_data_set/receptor_full_length_model_validation.fasta")
+
+
+
+
+
+
