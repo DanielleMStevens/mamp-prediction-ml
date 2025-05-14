6772d88cbc0dba975b84ac5e234ad9ea4361c9fe
Brian Su <briansuaccs@email.com>
2025-05-14 12:05:24 -0700

    update lrr extraction

--- 08_model_analysis/03_LRR_extraction.R
+++ 08_model_analysis/03_LRR_extraction.R
@@ -13,3 +13,15 @@ fasta_data <- data.frame(Header_Name = paste(">", train_immuno_stratify$Header_N
 # write fasta file
 write.table(fasta_data, file = "./08_model_analysis/LRR_domain_sequences_train_immuno_stratify.fasta", 
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\n")
+
+
+# prep Header_Name and Receptor Sequence for fasta file conversion and remove duplicates
+# Filter for FLS2 sequences only
+fasta_data <- data.frame(Header_Name = paste(">", train_immuno_stratify$Header_Name, sep = ""),
+                         Receptor_Sequence = train_immuno_stratify$Receptor.Sequence) %>%
+              filter(grepl("FLS2", Header_Name)) %>%
+              distinct(Header_Name, .keep_all = TRUE)
+
+# write fasta file
+write.table(fasta_data, file = "./08_model_analysis/LRR_domain_sequences_FLS2.fasta", 
+            quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\n")
