82063eadc0d9ea91528c76d32d9f917753e2d67d
Brian Su <briansuaccs@email.com>
2025-05-14 11:36:26 -0700

    update lrr extraction

--- 08_model_analysis/03_LRR_extraction.R
+++ 08_model_analysis/03_LRR_extraction.R
@@ -1,11 +1,14 @@
 # extract domain sequence information from train_immuno_stratify.csv
 
+library(tidyverse)
+
 # load train_immuno_stratify.csv
 train_immuno_stratify <- read.csv("./05_datasets/train_immuno_stratify.csv")
 
-# prep Header_Name and Receptor Sequence for fasta file conversion
+# prep Header_Name and Receptor Sequence for fasta file conversion and remove duplicates
 fasta_data <- data.frame(Header_Name = paste(">", train_immuno_stratify$Header_Name, sep = ""),
-                         Receptor_Sequence = train_immuno_stratify$Receptor.Sequence)
+                         Receptor_Sequence = train_immuno_stratify$Receptor.Sequence) %>%
+              distinct(Header_Name, .keep_all = TRUE)
 
 # write fasta file
 write.table(fasta_data, file = "./08_model_analysis/LRR_domain_sequences_train_immuno_stratify.fasta", 
