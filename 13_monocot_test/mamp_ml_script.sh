890a41f3b240edebaaf28dd72fae9cf79da5fa0d
Danielle Stevens <dmstev@berkeley.edu>
2026-06-24 12:09:04 -0700

    monocot mamp-ml outputs and reanalysis of wheat

---
+++ 13_monocot_test/mamp_ml_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=maize
+#SBATCH --account=ac_kvkallow
+#SBATCH --partition=savio3_gpu
+#SBATCH --qos=v100_gpu3_normal
+#SBATCH --gres=gpu:V100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=8:00:00
+#SBATCH --mail-user=dmstev@berkeley.edu
+#SBATCH --mail-type=ALL
+
+cd /global/scratch/users/dmstev/mamp_prediction_ml/13_monocot_test/
+
+module load anaconda3
+conda activate localfold
+export PATH="/global/scratch/users/dmstev/localcolabfold/colabfold-conda/bin:$PATH"
+
+mamp-ml predict Maize_FLS2_search.xlsx --device cuda 
